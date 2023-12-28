cd0a398efc8c3d03f12c6f7123fbcd321150db1f
shiba_1234 <shibajisahu121@gmail.com>
2023-12-28 23:32:26 +0530

    profile page top designed

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../ProfilePage/ProfilePage.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import StopClock from "../StopClock/StopClock";
 
 const ProfilePage = () => {
   const params = useParams();
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  const [countryList, setCountryList] = useState([]);
+  const [country, setCountry] = useState("Africa/Abidjan");
+
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${params.userId}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((user) => setUser(user))
+      .catch((error) => console.log(error));
+  }, [params]);
+
+  useEffect(() => {
+    const url = "http://worldtimeapi.org/api/timezone";
+    fetch(url)
+      .then((response) => response.json())
+      .then((list) => setCountryList(list))
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div>
-      <h1>ProfilePage</h1>
-      <h3>{params.userId}</h3>
+    <div className="profile-page">
+      <div className="profile-header">
+        <div className="back-btn">
+          <button type="submit" onClick={() => navigate("/")}>
+            Back
+          </button>
+        </div>
+        <div className="header-right">
+          <select onChange={(e) => setCountry(e.target.value)}>
+            {countryList.map((country, index) => {
+              return <option key={index}>{country}</option>;
+            })}
+          </select>
+          <StopClock country={country} />
+        </div>
+      </div>
+      <h1 className="profile-heading">Profile Page</h1>
+      <div className="profile-details">
+        <div className="details-left">
+          <h3>{user?.name}</h3>
+          <div className="ct-phrase">
+            {user?.username} | {user?.company?.catchPhrase}
+          </div>
+        </div>
+        <div className="details-right">
+          <div className="addr">
+            {user?.address?.street},{user?.address?.suite},{user?.address?.city}
+          </div>
+          <div className="email">
+            {user?.email} | {user?.phone}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/Components/StopClock/StopClock.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../StopClock/StopClock.css";
+
+const StopClock = ({ country }) => {
+  return (
+    <div>
+      <div>{country}</div>
+    </div>
+  );
+};
+
+export default StopClock;
