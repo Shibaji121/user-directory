8ddf3c510e7664f4f06ceace6581bb8b6daa96d4
shiba_1234 <shibajisahu121@gmail.com>
2023-12-29 15:15:26 +0530

    Stop Clock logic added successfully

--- src/Components/StopClock/StopClock.js
+++ src/Components/StopClock/StopClock.js
@@ -2,22 +2,71 @@ import React, { useEffect, useState } from "react";
 import "../StopClock/StopClock.css";
 
 const StopClock = ({ country }) => {
-  const [datetime, setDatetime] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const [hour, setHour] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [second, setSecond] = useState("00");
 
   useEffect(() => {
-    const url = `http://worldtimeapi.org/api/timezone/${country}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((time) => setDatetime(time?.datetime))
-      .catch((error) => console.log(error));
+    const fetchData = async () => {
+      let timedate;
+      try {
+        const url = `http://worldtimeapi.org/api/timezone/${country}`;
+        const response = await fetch(url);
+        timedate = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+      const timeStr = timedate?.datetime.toString();
+      const time = timeStr
+        .substring(timeStr.indexOf("T") + 1, timeStr.indexOf("T") + 9)
+        .split(":");
+      const hour = parseInt(time[0], 10);
+      const minute = parseInt(time[1], 10);
+      const second = parseInt(time[2], 10);
+      setHour(hour.toString().padStart(2, "0"));
+      setMinute(minute.toString().padStart(2, "0"));
+      setSecond(second.toString().padStart(2, "0"));
+    };
+    fetchData();
   }, [country]);
-  const timeStr = datetime.toString();
-  const time = timeStr
-    .substring(timeStr.indexOf("T") + 1, timeStr.indexOf("T") + 9)
-    .split(":");
-  const hour = time[0];
-  const minute = time[1];
-  const second = time[2];
+
+  useEffect(() => {
+    const runTimer = () => {
+      let intSecond = parseInt(second, 10);
+      let intMinute = parseInt(minute, 10);
+      let intHour = parseInt(hour, 10);
+      if (!isPaused) {
+        intSecond = intSecond + 1;
+        if (intSecond === 60) {
+          intSecond = 0;
+          intMinute = intMinute + 1;
+        }
+        if (intMinute === 60) {
+          intMinute = 0;
+          intHour = intHour + 1;
+        }
+        if (intHour === 24) {
+          intHour = 0;
+        }
+      }
+      setHour(intHour.toString().padStart(2, "0"));
+      setMinute(intMinute.toString().padStart(2, "0"));
+      setSecond(intSecond.toString().padStart(2, "0"));
+    };
+
+    const intervalId = setInterval(runTimer, 1000);
+    if (isPaused) {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [minute, second, hour, isPaused]);
+
+  const togglePause = () => {
+    setIsPaused((prevIsPaused) => {
+      return !prevIsPaused;
+    });
+  };
 
   return (
     <div className="clock-container">
@@ -26,7 +75,9 @@ const StopClock = ({ country }) => {
           {hour} : {minute} : {second}
         </div>
       </div>
-      <button type="submit">Pause/Start</button>
+      <button type="button" onClick={togglePause}>
+        {isPaused ? "Start" : "Pause"}
+      </button>
     </div>
   );
 };
