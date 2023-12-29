3c28f6b50c0a58e5d64dea6004af6a84e9646850
shiba_1234 <shibajisahu121@gmail.com>
2023-12-29 18:57:19 +0530

    Pop up implemented

---
+++ src/Components/Popup/Popup.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+import "../Popup/Popup.css";
+
+export default function Popup({ handleClose, children }) {
+  const popupRef = useRef();
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      console.log(popupRef);
+      console.log(popupRef.current);
+      console.log(event.target);
+      console.log(event.target.contains(popupRef.current));
+      if (event.target.contains(popupRef.current)) {
+        handleClose();
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [handleClose]);
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={handleClose}>
+          &times;
+        </span>
+        <div ref={popupRef}>{children}</div>
+      </div>
+    </div>
+  );
+}
--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../PostList/PostList.css";
+import Popup from "../Popup/Popup";
 
 const PostList = ({ userId }) => {
   const [postList, setPostList] = useState([]);
+  const [isPostClick, setIsPostClick] = useState(false);
+  const [post, setPost] = useState();
 
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/users/${userId}/posts`;
@@ -11,17 +14,43 @@ const PostList = ({ userId }) => {
       .then((list) => setPostList(list))
       .catch((error) => console.log(error));
   }, [userId]);
+
+  const handlePostClick = (post) => {
+    console.log("click");
+    setIsPostClick(true);
+    setPost(post);
+  };
+
+  const handleClose = () => {
+    console.log("close");
+    setIsPostClick(false);
+  };
+
   return (
-    <div className="postlist-container">
-      {postList.map((post) => {
-        return (
-          <div key={post?.id} className="post-container">
+    <>
+      <div className="postlist-container">
+        {postList.map((post) => {
+          return (
+            <div
+              key={post?.id}
+              className="post-container"
+              onClick={() => handlePostClick(post)}
+            >
+              <h4 className="post-title">{post?.title}</h4>
+              <div className="post-body">{post?.body}</div>
+            </div>
+          );
+        })}
+      </div>
+      {isPostClick && (
+        <Popup handleClose={handleClose}>
+          <div className="post-container">
             <h4 className="post-title">{post?.title}</h4>
             <div className="post-body">{post?.body}</div>
           </div>
-        );
-      })}
-    </div>
+        </Popup>
+      )}
+    </>
   );
 };
 
